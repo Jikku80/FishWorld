f7c66e3c7676554a810d8d9a0ed97ad85fc6e269
Jeshan Maharjan <47115629+Jikku80@users.noreply.github.com>
2023-12-22 10:33:43 +0545

    credential added

--- util/sqldb.js
+++ util/sqldb.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize").Sequelize;
 
-const sequelize = new Sequelize("fishStore","root","",{dialect: "mysql", host: "localhost", password: "password"});
+const sequelize = new Sequelize("fishStore","admin","",{dialect: "mysql", host: "assesmentdb.cvquzkjart0o.us-east-1.rds.amazonaws.com", password: "jikku1234"});
 
 module.exports = sequelize;
